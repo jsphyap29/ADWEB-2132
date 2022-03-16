b376575188bf710647e56416cf17e36e40bfb896
jsphyap29 <84338201+jsphyap29@users.noreply.github.com>
2022-03-16 13:01:15 +0800

    Add files via upload

---
+++ Midterm Hands-on Quiz Sharing Data Across Components/app/emplist2/emplist2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emplist2',
+  templateUrl: './emplist2.component.html',
+  styleUrls: ['./emplist2.component.css']
+})
+export class Emplist2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
